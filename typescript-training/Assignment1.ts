b40c4c761325023a0231671a866be362a41b93c8
Swaroopa Mandapati <mandapatiswaroopa@gmail.com>
2026-04-17 22:56:51 +0530

    Assignment1 modified

--- typescript-training/Assignment1.ts
+++ typescript-training/Assignment1.ts
@@ -1,23 +1,23 @@
 //Temperature of a city in degrees Celsius: 25.5
 let temperature : number = 25.5;
-console.log("Temperature of a city in degrees Celsius : " + temperature);
+console.log(`Temperature of a city in degrees Celsius : ${temperature}`);
 
 //Whether a customer has placed an order: true or false
 let order : boolean =true;
-console.log("Did the customer placed an order: " + true );
+console.log(`Did the customer placed an order: ${order}` );
 
 //Person's phone number: "123-456-7890"
 let phoneNumber : string = "123-456-7890";
-console.log("Person's phone number: " + phoneNumber );
+console.log(`Person's phone number: ${phoneNumber}`);
 
 //Amount of money in a customer's bank account: 1000.50
 
 let amount : number = 2000.698;
-console.log("Amount of money in a customer's bank account: " + amount);
+console.log(`Amount of money in a customer's bank account: ${amount}`);
 
 //Person's email address: "john.doe@example.com"
 let emailAddress : string = "john.doe@example.com";
-console.log("Person's email address: "+ emailAddress);
+console.log(`Person's email address: ${emailAddress}`);
 
 //Coordinates of a location (latitude, longitude): 37.7749, -122.4194
 interface locationOfCity {
@@ -28,55 +28,57 @@ let coOrdinatesOfCity:locationOfCity={
         latitude:37.7749,
         longitude:"-122.4194"
     }
-    console.log("Coordinates of a location (latitude, longitude):"+coOrdinatesOfCity.latitude+", "+coOrdinatesOfCity.longitude);
+    console.log(`Coordinates of a location (latitude, longitude):${coOrdinatesOfCity.latitude},${coOrdinatesOfCity.longitude}`);
     
 // Person's marital status: true or false
-let maritalStatus : boolean =true;
-console.log("Person's marital status : " +maritalStatus);
+let maritalStatus : boolean =true;//console.log("Person's marital status : " +maritalStatus);
+console.log(`Person's marital status :  ${maritalStatus}`);
 
 // Person's occupation: "Software Engineer"
 let occupationOfPerson : boolean =true;
-console.log("Person's occupation : " +occupationOfPerson );
+console.log(`Person's occupation : ${occupationOfPerson}` );
 // Person's favourite colour: "Blue"
 let favouriteColor : string="Blue";
-console.log("Person's favourite colour: "+favouriteColor);
+console.log(`Person's favourite colour: ${favouriteColor}`);
 // Current year: 2023
 const currentYear : number= new Date().getFullYear();
-console.log("Current Year is :"+ currentYear);
+console.log(`Current Year is :${currentYear}`);
 //Number of followers on a social media platform: 1,000,000
 let followersOnSocialMedia : string = "1,000,000";
-console.log("Number of followers on a social media platform :" + followersOnSocialMedia);
+console.log(`Number of followers on a social media platform :${followersOnSocialMedia}`);
 
 //Rating of a movie: 7.5
 let ratingOfMovie : number =7.5;
-console.log("Rating of a Movie : "+ ratingOfMovie);
+console.log(`Rating of a Movie : ${ratingOfMovie}`);
+
 //Person's blood type: 'A'
 let bloodType : string ="A";
-console.log("Person's bloodType :"+ bloodType);
+console.log(`Person's bloodType : ${bloodType}`);
 
 //Title of a book: "To Kill a Mockingbird"
 let bookTitle : string="To Kill a Mockingbird";
-console.log("Title of a book:"+ bookTitle);
+console.log(`Title of a book: ${bookTitle}`);
 
 //Number of employees in a company: 500
 let numberOfEmployees : number=500;
-console.log("Number of employees in a company :"+ numberOfEmployees);
+console.log(`Number of employees in a company : ${numberOfEmployees}`);
 
 //Time of an event: 2:30 PM
 const eventTime : string ="2.30 PM";
-console.log("Time of an event : "+ eventTime);
+console.log(`Time of an event :  ${eventTime}`);
 
 //Name of a country: "United States"
 let countryName: string="United States";
-console.log("Name of a country:"+ countryName);
+console.log(`Name of a country: ${countryName}`);
 //Person's eye color: "Brown"
 let colorOfEye : string="Brown";
-console.log("Person's eye color :"+colorOfEye);
+console.log(`Person's eye color : ${colorOfEye}`);
 //Person's birthplace: "New York City"
 let birthPlace : string="New York City";
-console.log("Person's birth place :"+birthPlace);
+console.log(`Person's birth place :${birthPlace}`);
 // Distance between two cities: 200.5
 const distanceBetweenCities : number=200.5;
-console.log("Distance between two cities : "+ distanceBetweenCities);
+console.log(`Distance between two cities : ${distanceBetweenCities}`);
+
 
 
