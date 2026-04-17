bf03bfd38222e5af821b3752b7eb12385a2bf100
Swaroopa Mandapati <mandapatiswaroopa@gmail.com>
2026-04-17 21:35:23 +0530

    Assignment1

---
+++ javascript-training/language-basics/operator.js
@@ -0,0 +1,18 @@
+// Syntax to store data" Declaration Variable=Data;
+
+// Operators==> Operators are special chars used to perform  operations in js
+
+// There are 5 types of operators in js
+//1. Arithmetic Operators
+//2. Assignment Operators
+//3. Comparison Operators
+// 4. Logical Operators
+//5. Ternary Operators
+
+//1. Arithmetic Operators: The special chars used to perform mathematical operations in js(+,-,*,/,%,++,--)
+let a=10;
+let b=20;
+a=a++;
+
+console.log(a++);
+console.log(b--);
